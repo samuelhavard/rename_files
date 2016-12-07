62b21dc7d95c7600b3d87d50654b9565ad9bf5df
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-07 12:55:40 -0500

    initial commit

--- rename_files.py
+++ rename_files.py
@@ -7,6 +7,8 @@ def rename_files():
 
     os.chdir(r"C:\Users\samue_000\Desktop\udacity\Python\50 photos\prank")
     for file_name in file_list:
+        print("Old Name - " + file_name)
+        print("New Name - " + file_name.translate(None, "0123456789"))
         os.rename(file_name, file_name.translate(None, "0123456789"))
     os.chdir(saved_path)
 
